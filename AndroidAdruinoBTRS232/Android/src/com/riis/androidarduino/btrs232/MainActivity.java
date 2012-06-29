d975012dad07e9a2d39ed1ae6c36b6ccebf4ebf8
Austin Mueller <armueller@tabusnine.com>
2012-06-29 15:55:23 -0400

    Initial commit for AndroidArduinoBTRS232 app
    Android communication via bluetooth to the Arduino, then Arduino
    communication via RS232 to… anything else.

---
+++ AndroidAdruinoBTRS232/Android/src/com/riis/androidarduino/btrs232/MainActivity.java
@@ -0,0 +1,96 @@
+package com.riis.androidarduino.btrs232;
+
+import com.riis.androidarduino.btrs232.R;
+import com.riis.androidarduino.lib.BlueToothComm;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+	private static String DEVICE_NAME = "AndroidArduinoBT";
+	
+	private Button connectButton;
+	private Button disconnectButton;
+	
+	private EditText msgBox;
+	private Button sendMsgButton;
+
+	
+	
+	private BlueToothComm btComm;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        setUpGUI();
+    }
+    
+    private void setUpGUI() {
+    	setUpConnectButton();
+    	setUpDisconnectButton();
+    	setupMsgBox();
+    	setupSendMsgButton();
+	}
+    
+    private void setupMsgBox() {
+    	msgBox = (EditText) findViewById(R.id.messageBox);
+    }
+    
+    private void setupSendMsgButton() {
+    	sendMsgButton = (Button) findViewById(R.id.sendButton);
+    	sendMsgButton.setOnClickListener(
+    	    		new OnClickListener(){
+    					public void onClick(View v) {
+    						btComm.sendString(msgBox.getText().toString());
+    						msgBox.setText("");
+    					}
+    	    		}
+    	    	);
+    }
+    
+    @Override
+	public void onResume() {
+		super.onResume();
+		
+		if(btComm == null) {
+			btComm = new BlueToothComm(this, DEVICE_NAME);
+		} else {
+			btComm.resumeConnection();
+		}
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		btComm.pauseConnection();
+	}
+    
+    private void setUpConnectButton() {
+    	connectButton = (Button)findViewById(R.id.connectButton);
+    	connectButton.setOnClickListener(
+    		new OnClickListener() {
+    			public void onClick(View v) {
+    				btComm.connect();
+    			}
+    		}
+    	);
+    }
+    
+    private void setUpDisconnectButton() {
+    	disconnectButton = (Button)findViewById(R.id.disconnectButton);
+    	disconnectButton.setOnClickListener(
+    		new OnClickListener() {
+    			public void onClick(View v) {
+    				btComm.disconnect();
+    			}
+    		}
+    	);
+    }
+}
