4009167933b12f3238a9ebbaee6df8df179288e0
Ben Oztalay <bmoztalay@tabusnine.com>
2012-07-24 12:33:35 -0400

    Finally got a test project working

---
+++ libs/ExternalDeviceTest/src/com/riis/androidarduino/lib/test/BluetoothCommTest.java
@@ -0,0 +1,95 @@
+package com.riis.androidarduino.lib.test;
+
+import static org.junit.Assert.fail;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import android.bluetooth.BluetoothDevice;
+
+import com.riis.androidarduino.lib.BluetoothComm;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ BluetoothComm.class })
+public class BluetoothCommTest {
+
+	@Test
+	public void testBluetoothComm() {
+//		mockStatic(BluetoothDevice.class);
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConnect() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisconnect() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPauseConnection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testResumeConnection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSendString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSendByteWithFlag() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWrite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearMessages() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsMessageReady() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadMessageWithFlags() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRead() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShouldPrintLogMsgs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsConnected() {
+		fail("Not yet implemented");
+	}
+
+}
