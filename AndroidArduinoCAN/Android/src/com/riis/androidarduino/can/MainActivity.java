6be335c877f7ed1006809e8eb5a498ce92409072
Austin Mueller <armueller@tabusnine.com>
2012-07-27 16:50:25 -0400

    Finished first draft of the CAN android app

--- AndroidArduinoCAN/Android/src/com/riis/androidarduino/can/MainActivity.java
+++ AndroidArduinoCAN/Android/src/com/riis/androidarduino/can/MainActivity.java
@@ -1,5 +1,6 @@
 package com.riis.androidarduino.can;
 import java.util.ArrayList;
+import java.util.Random;
 
 import com.riis.androidarduino.lib.BluetoothComm;
 
@@ -14,6 +15,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
+import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -22,10 +24,31 @@ public class MainActivity extends Activity {
 	private static final int MAX_ARRAY_SIZE = 1000;
 	
 	private Button btConnectButton;
-	private Button btDisconnectButton;
-	private Button startTracking;
-	private Button stopTracking;
-	private Button pauseTracking;
+	private Button startTrackingButton;
+	private Button stopTrackingButton;
+	private Button pauseTrackingButton;
+	
+	private TextView engineRunTime;
+	private TextView airTemp;
+	private TextView hybridBatteryPack;
+	private TextView VIN;
+	
+	private ProgressBar oilTempBar;
+	private TextView oilTempTxt;
+	private ProgressBar coolantTempBar;
+	private TextView coolantTempTxt;
+	private ProgressBar throttlePosBar;
+	private TextView throttlePosTxt;
+	private ProgressBar absThrottleBBar;
+	private TextView absThrottleBTxt;
+	private ProgressBar absThrottleCBar;
+	private TextView absThrottleCTxt;
+	private ProgressBar absAccPosDBar;
+	private TextView absAccPosDTxt;
+	private ProgressBar absAccPosEBar;
+	private TextView absAccPosETxt;
+	private ProgressBar absAccPosFBar;
+	private TextView absAccPosFTxt;
 	
 	private volatile boolean keepRunning;
 	private boolean lastStatus;
@@ -43,20 +66,18 @@ public class MainActivity extends Activity {
 				if(btComm.isConnected()) {
 					if(!lastStatus) {
 						lastStatus = true;
-						appendMsgToMsgLog("Bluetooth Connected!");
+						changeConnectButtonText();
 					}
 					
 					if(btComm.isMessageReady()) {
 						String data = btComm.readMessage();
-						appendMsgToMsgLog("Bluetooth data: " + data);
 						parseCANData(data);
 					}
 		        	
 		        } else {
 		        	if(lastStatus) {
 						lastStatus = false;
-						appendMsgToMsgLog("Bluetooth Disconnected!");
-						appendMsgToMsgLog("Waiting for Bluetooth connection...");
+						changeConnectButtonText();
 					}
 		        }
 				
@@ -87,31 +108,135 @@ public class MainActivity extends Activity {
     
     private void setUpGUI() {
     	setUpConnectButton();
-    	setUpDisconnectButton();
+    	setUpStartTrackButton();
+    	setUpPauseTrackButton();
+    	setUpStopTrackButton();
+    	setUpTempMonitors();
+    	setUpThrottleMonitors();
+    	setUpAcceleratorMonitors();
+    	setUpBatteryMonitor();
+    	setUpRuntimeMonitor();
+    	setUpVIN();
     	setupHandler();
 	}
-    
-    private void setUpConnectButton() {
+
+	private void setUpConnectButton() {
     	btConnectButton = (Button)findViewById(R.id.connectButton);
     	btConnectButton.setOnClickListener(
     		new OnClickListener() {
     			public void onClick(View v) {
-    				btComm.connect();
+    				if(btComm.isConnected())
+    					btComm.disconnect();
+    				else
+    					btComm.connect();
     			}
     		}
     	);
     }
+	
+	private void changeConnectButtonText() {
+		if(btComm.isConnected())
+			btConnectButton.setText("Disconnect");
+		else
+			btConnectButton.setText("Connect");
+	}
     
-    private void setUpDisconnectButton() {
-    	btDisconnectButton = (Button)findViewById(R.id.disconnectButton);
-    	btDisconnectButton.setOnClickListener(
+    private void setUpStartTrackButton() {
+    	startTrackingButton = (Button)findViewById(R.id.startTrackButton);
+    	startTrackingButton.setOnClickListener(
     		new OnClickListener() {
     			public void onClick(View v) {
-    				btComm.disconnect();
+    				//TODO Start tracking data values
     			}
     		}
     	);
-    }
+	}
+
+	private void setUpPauseTrackButton() {
+		pauseTrackingButton = (Button)findViewById(R.id.pauseTrackButton);
+    	pauseTrackingButton.setOnClickListener(
+    		new OnClickListener() {
+    			public void onClick(View v) {
+    				//TODO Pause tracking data values
+    			}
+    		}
+    	);
+	}
+
+	private void setUpStopTrackButton() {
+		stopTrackingButton = (Button)findViewById(R.id.stopTrackButton);
+    	stopTrackingButton.setOnClickListener(
+    		new OnClickListener() {
+    			public void onClick(View v) {
+    				//TODO Stop tracking data values
+    			}
+    		}
+    	);
+	}
+
+	private void setUpTempMonitors() {
+		oilTempBar = (ProgressBar)findViewById(R.id.oilTempBar);
+		oilTempBar.setMax(255);
+		oilTempBar.setProgress(40);
+		oilTempTxt = (TextView)findViewById(R.id.oilTempTxt);
+		oilTempTxt.append("0 C");
+		
+		coolantTempBar = (ProgressBar)findViewById(R.id.coolantTempBar);
+		coolantTempBar.setMax(255);
+		coolantTempBar.setProgress(40);
+		coolantTempTxt = (TextView)findViewById(R.id.coolantTempTxt);
+		coolantTempTxt.append("0 C");
+		
+		airTemp = (TextView)findViewById(R.id.airTemp);
+		airTemp.append("0 C");
+	}
+
+	private void setUpThrottleMonitors() {
+		throttlePosBar = (ProgressBar)findViewById(R.id.throttlePosBar);
+		throttlePosBar.setMax(100);
+		throttlePosTxt = (TextView)findViewById(R.id.throttlePosTxt);
+		throttlePosTxt.append("0%");
+		
+		absThrottleBBar = (ProgressBar)findViewById(R.id.absoluteThrottleBBar);
+		absThrottleBBar.setMax(100);
+		absThrottleBTxt = (TextView)findViewById(R.id.absoluteThrottleBTxt);
+		absThrottleBTxt.append("0%");
+		
+		absThrottleCBar = (ProgressBar)findViewById(R.id.absoluteThrottleCBar);
+		absThrottleCBar.setMax(100);
+		absThrottleCTxt = (TextView)findViewById(R.id.absoluteThrottleCTxt);
+		absThrottleCTxt.append("0%");
+	}
+
+	private void setUpAcceleratorMonitors() {
+		absAccPosDBar = (ProgressBar)findViewById(R.id.absAccPosDBar);
+		absAccPosDBar.setMax(100);
+		absAccPosDTxt = (TextView)findViewById(R.id.absAccPosDTxt);
+		absAccPosDTxt.append("0%");
+		
+		absAccPosEBar = (ProgressBar)findViewById(R.id.absAccPosEBar);
+		absAccPosEBar.setMax(100);
+		absAccPosETxt = (TextView)findViewById(R.id.absAccPosETxt);
+		absAccPosETxt.append("0%");
+		
+		absAccPosFBar = (ProgressBar)findViewById(R.id.absAccPosFBar);
+		absAccPosFBar.setMax(100);
+		absAccPosFTxt = (TextView)findViewById(R.id.absAccPosFTxt);
+		absAccPosFTxt.append("0%");
+	}
+
+	private void setUpBatteryMonitor() {
+		hybridBatteryPack = (TextView)findViewById(R.id.hybridBatteryPack);
+	}
+	
+	private void setUpVIN() {
+		VIN = (TextView)findViewById(R.id.VIN);
+	}
+	
+	private void setUpRuntimeMonitor() {
+		engineRunTime = (TextView)findViewById(R.id.engineRunTime);
+		engineRunTime.append("0s");
+	}
     
     private void setupHandler() {
 		handler = new Handler() {
@@ -123,25 +248,125 @@ public class MainActivity extends Activity {
 				if(tokens[0].equals("LOG")) {
 					
 				} else if(tokens[0].equals("DATA")) {
-					//TODO parse data to determine if it's engine rpm, vehicle speed, engine temp, etc...
-//					float reading = Float.parseFloat(message);
-//					if(readings.size() == MAX_ARRAY_SIZE)
-//						readings.remove(0);
-//					readings.add(reading);
-//					
-//					if(readings.size() % 10 == 0) {
-//						((LinearLayout)findViewById(R.id.graphContainer)).removeAllViews();
-//						setUpGraph();
-//					}
+			    	if(tokens[1].charAt(0) == 0x02)
+			    		setVIN(tokens[1].substring(1));
+			    	else if(tokens[1].charAt(0) == 0x04)
+			    		setEngineLoadVal(tokens[1].substring(1));
+			    	else if(tokens[1].charAt(0) == 0x05)
+			    		setEngineCoolantTemp(tokens[1].substring(1));
+			    	else if(tokens[1].charAt(0) == 0x0C)
+			    		setEngineRPM(tokens[1].substring(1));
+			    	else if(tokens[1].charAt(0) == 0x0D)
+			    		setVehicleSpeed(tokens[1].substring(1));
+			    	else if(tokens[1].charAt(0) == 0x11)
+			    		setThrottlePosition(tokens[1].substring(1));
+			    	else if(tokens[1].charAt(0) == 0x1F)
+			    		setEngineRunTime(tokens[1].substring(1));
+			    	else if(tokens[1].charAt(0) == 0x2F)
+			    		setFuelLevelInput(tokens[1].substring(1));
+			    	else if(tokens[1].charAt(0) == 0x46)
+			    		setAmbiantAirTemp(tokens[1].substring(1));
+			    	else if(tokens[1].charAt(0) == 0x47)
+			    		setAbsoluteThrottleB(tokens[1].substring(1));
+			    	else if(tokens[1].charAt(0) == 0x48)
+			    		setAbsoluteThrottleC(tokens[1].substring(1));
+			    	else if(tokens[1].charAt(0) == 0x49)
+			    		setAccPedalPosD(tokens[1].substring(1));
+			    	else if(tokens[1].charAt(0) == 0x4A)
+			    		setAccPedalPosE(tokens[1].substring(1));
+			    	else if(tokens[1].charAt(0) == 0x4B)
+			    		setAbsAccPedalPosF(tokens[1].substring(1));
+			    	else if(tokens[1].charAt(0) == 0x5B)
+			    		setHybridBatteryPackLife(tokens[1].substring(1));
+			    	else if(tokens[1].charAt(0) == 0x5C)
+			    		setEngineOilTemp(tokens[1].substring(1));
 				}
 			}
 		};
 	}
     
-    private void appendMsgToMsgLog(String str) {
-		Message msg = Message.obtain(handler);
-		msg.obj = "LOG~" + str;
-		handler.sendMessage(msg);
+    private void setVIN(String vin) {
+    	VIN.setText(R.string.VINPreface + vin);
+	}
+
+	private void setEngineLoadVal(String substring) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void setEngineCoolantTemp(String coolantTempStr) {
+    	int coolantTemp = Integer.parseInt(coolantTempStr);
+		coolantTempTxt.setText(R.string.engineCoolantTempPreface + coolantTemp + " C");
+		coolantTempBar.setProgress(coolantTemp+40);
+	}
+
+	private void setEngineRPM(String substring) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void setVehicleSpeed(String substring) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void setThrottlePosition(String substring) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void setEngineRunTime(String timeStr) {
+		engineRunTime.setText(R.string.engineRunTimePreface + timeStr + "s");
+	}
+
+	private void setFuelLevelInput(String substring) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void setAmbiantAirTemp(String tempStr) {
+		airTemp.setText(R.string.airTempPreface + tempStr + " C");
+	}
+
+	private void setAbsoluteThrottleB(String absThrottleBStr) {
+		int absThrottleB = Integer.parseInt(absThrottleBStr);
+		absThrottleBTxt.setText(R.string.absThrottleBPreface + absThrottleB + "%");
+		absThrottleBBar.setProgress(absThrottleB);
+	}
+
+	private void setAbsoluteThrottleC(String absThrottleCStr) {
+		int absThrottleC = Integer.parseInt(absThrottleCStr);
+		absThrottleCTxt.setText(R.string.absThrottleCPreface + absThrottleC + "%");
+		absThrottleCBar.setProgress(absThrottleC);
+	}
+
+	private void setAccPedalPosD(String absAccPosDStr) {
+		int absAccPosD = Integer.parseInt(absAccPosDStr);
+		absAccPosDTxt.setText(R.string.accPedalPosDPreface + absAccPosD + "%");
+		absAccPosDBar.setProgress(absAccPosD);
+	}
+
+	private void setAccPedalPosE(String absAccPosEStr) {
+		int absAccPosE = Integer.parseInt(absAccPosEStr);
+		absAccPosETxt.setText(R.string.accPedalPosEPreface + absAccPosE + "%");
+		absAccPosEBar.setProgress(absAccPosE);
+	}
+
+	private void setAbsAccPedalPosF(String absAccPosFStr) {
+		int absAccPosF = Integer.parseInt(absAccPosFStr);
+		absAccPosFTxt.setText(R.string.accPedalPosFPreface + absAccPosF + "%");
+		absAccPosFBar.setProgress(absAccPosF);
+	}
+
+	private void setHybridBatteryPackLife(String batteryLifeStr) {
+		int batteryLife = Integer.parseInt(batteryLifeStr);
+		hybridBatteryPack.setText(R.string.hybridBatteryPackPreface + batteryLife + "%");
+	}
+
+	private void setEngineOilTemp(String oilTempStr) {
+    	int oilTemp = Integer.parseInt(oilTempStr);
+		oilTempTxt.setText(R.string.engineOilTempPreface + oilTemp + " C");
+		oilTempBar.setProgress(oilTemp+40);
     }
     
     private void parseCANData(String data) {
